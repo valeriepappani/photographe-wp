8ca69594de3682a6e4e02cea6aef194aae6f1329
Valerie Pappani <v.pappani@smallstories.fr>
2024-10-28 15:26:59 +0100

    ajout header + footer

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+			<footer id="site-footer" class="header-footer-group footer-NM">
+                <ul>
+					<li><a href="#">Mentions légales </a></li>
+					<li><a href="#">Vie privée </a></li>
+					<li><a href="#">Tous droits réservés</a></li>
+				</ul>
+			</footer>
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
